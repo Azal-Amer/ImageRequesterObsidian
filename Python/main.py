e3022273e93ea88d041f5c803692cafa04f82ca7
Azal-Amer <azalamer6@gmail.com>
2024-07-09 16:32:01 -0500

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -28,7 +28,7 @@ def set_webhook(bot_token, webhook_url):
         print('Error:', response.text)
 
 
-webhook_url = 'https://41f5-140-82-220-240.ngrok-free.app'
+webhook_url = 'https://server.azalamer.com'
 # this needs us to use ngrok, note that we need to find a way to keep this constant, 
 # or push it to my domain url as a subaddress
 
